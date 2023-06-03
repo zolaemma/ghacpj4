14c8457939d5d1d1383e5b6dcf819bae24e9a6b8
zolaemma <zolaemma@gmail.com>
2023-06-02 23:41:20 -0400

    adding port 8080

--- main.tf
+++ main.tf
@@ -52,8 +52,8 @@ resource "aws_key_pair" "ghac-pj7" {
   
 
   egress {
-    from_port        = 0
-    to_port          = 0
+    from_port        = 8080
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
    
