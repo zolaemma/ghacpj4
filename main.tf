234762263834bc2cf8ebaad8fe7168ac50afdf9e
zolaemma <zolaemma@gmail.com>
2023-06-02 21:41:06 -0400

    adding port 8080

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #creating the key pair
 
-resource "aws_key_pair" "ghac-pj6" {
-  key_name   = "ghac-pj6"
+resource "aws_key_pair" "ghac-pj7" {
+  key_name   = "ghac-pj7"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGIoggCg6hyYtgMXPbEkqb7e4WHDOgoUEKofmqjw7oVcsLTJ6vJei9LrDvgLmGXSE9FVWtWkLbbrXmuU8CF2uEzJr+WSftuAmY+2ComlYigWfrzlPZsMpE572IjhbX94InNbblr1biSs3+XvV+hTY0yUN8m1G7vqpPt5Guvj3CtsOXPzDDI/PGEktgdfTliZdTjs6xhAOOpetS4dErINvByUyV48a+YE67pZqf6AeEbhdIhDb1xHfWWduBcp+Hg+TtoIrqNBLlDZKlaArKfyvfku+T7DJMRNwtkMYTjViK2ZLrzvkGFh/z1r8s/+LVVoLXvDbjCov8Ta8jAJ2cc3Ep+xUqAajYPW99sYK0QYZ+VZACkx8B5s71+fkWdVDsJQXa1EOrD7cpqCM8e+Zmx7dU+2TlMak7uwwoAaDxtJtiUqj0nF0bGU+04WmWX8yHgLcD2K+GrPihcnQhz5PIQD2D4ta5i5+eGoZC8xX/MajEOADkbfhn7kWzp5b9rl0DiJ0= zolae@Zola-Mac.local"
 
 }
@@ -13,8 +13,8 @@ resource "aws_key_pair" "ghac-pj6" {
 
   #creating security groups
 
-  resource "aws_security_group" "ghac-pj6" {
-  name        = "ghac-pj6"
+  resource "aws_security_group" "ghac-pj7" {
+  name        = "ghac-pj7"
   description = "Allow TLS inbound traffic"
   vpc_id = data.aws_vpc.default.id
   
@@ -60,21 +60,21 @@ resource "aws_key_pair" "ghac-pj6" {
   } 
 
   tags = {
-    Name = "ghac-pj6"
+    Name = "ghac-pj7"
   
   }
 }
 
 #creating an ec2 instrance
 
-resource "aws_instance" "ghac-pj6" {
+resource "aws_instance" "ghac-pj7" {
   ami = "ami-0261755bbcb8c4a84"
   instance_type = "t2.micro"
-  key_name = "ghac-pj6"
-  vpc_security_group_ids = [aws_security_group.ghac-pj6.id]
+  key_name = "ghac-pj7"
+  vpc_security_group_ids = [aws_security_group.ghac-pj7.id]
 
 
   tags = {
-    Name = "ghac-pj6"
+    Name = "ghac-pj7"
   }
  } 
