9d37facbc63fb9bf5d0150c2f545e743e56990e1
zolaemma <zolaemma@gmail.com>
2023-06-04 14:12:34 -0400

    adding backend

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #creating the key pair
 
-resource "aws_key_pair" "ghac-pj2" {
-  key_name   = "ghac-pj2"
+resource "aws_key_pair" "ghac-pj1" {
+  key_name   = "ghac-pj1"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGIoggCg6hyYtgMXPbEkqb7e4WHDOgoUEKofmqjw7oVcsLTJ6vJei9LrDvgLmGXSE9FVWtWkLbbrXmuU8CF2uEzJr+WSftuAmY+2ComlYigWfrzlPZsMpE572IjhbX94InNbblr1biSs3+XvV+hTY0yUN8m1G7vqpPt5Guvj3CtsOXPzDDI/PGEktgdfTliZdTjs6xhAOOpetS4dErINvByUyV48a+YE67pZqf6AeEbhdIhDb1xHfWWduBcp+Hg+TtoIrqNBLlDZKlaArKfyvfku+T7DJMRNwtkMYTjViK2ZLrzvkGFh/z1r8s/+LVVoLXvDbjCov8Ta8jAJ2cc3Ep+xUqAajYPW99sYK0QYZ+VZACkx8B5s71+fkWdVDsJQXa1EOrD7cpqCM8e+Zmx7dU+2TlMak7uwwoAaDxtJtiUqj0nF0bGU+04WmWX8yHgLcD2K+GrPihcnQhz5PIQD2D4ta5i5+eGoZC8xX/MajEOADkbfhn7kWzp5b9rl0DiJ0= zolae@Zola-Mac.local"
 
 }
@@ -13,8 +13,8 @@ data "aws_vpc" "default" {
 
   #creating security groups
 
-  resource "aws_security_group" "ghac-pj2" {
-  name = "ghac-pj2"
+  resource "aws_security_group" "ghac-pj1" {
+  name = "ghac-pj1"
   description = "Allow TLS inbound traffic"
   vpc_id = data.aws_vpc.default.id
   
@@ -63,22 +63,22 @@ egress {
   } 
 
   tags = {
-    Name = "ghac-pj2"
+    Name = "ghac-pj1"
   
   }
 }
 
 #creating an ec2 instrance
 
-resource "aws_instance" "ghac-pj2" {
+resource "aws_instance" "ghac-pj1" {
   ami = "ami-0261755bbcb8c4a84"
   instance_type = "t2.micro"
-  key_name = "ghac-pj2"
-  vpc_security_group_ids = [aws_security_group.ghac-pj2.id ]
+  key_name = "ghac-pj1"
+  vpc_security_group_ids = [aws_security_group.ghac-pj1.id ]
 
 
   tags = {
-    Name = "ghac-pj2"
+    Name = "ghac-pj1"
   }
 }
 
